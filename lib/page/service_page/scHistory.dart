c56780a0281f7e9d794f6b752c9ce52857a574c3
HaNeefz <47207756+HaNeefz@users.noreply.github.com>
2019-07-01 10:09:44 +0700

    editHistoryPage

--- lib/page/service_page/scHistory.dart
+++ lib/page/service_page/scHistory.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:warranzy_demo/tools/config/text_style.dart';
 import 'package:warranzy_demo/tools/widget_ui_custom/text_builder.dart';
 
 class HistoryPage extends StatefulWidget {
@@ -9,9 +10,22 @@ class HistoryPage extends StatefulWidget {
 class _HistoryPageState extends State<HistoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(child: TextBuilder.build(title: "History")),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: <Widget>[
+              TextBuilder.build(title: "Popular",style: TextStyleCustom.STYLE_LABEL_BOLD),
+              TextBuilder.build(title: "Electronics",style: TextStyleCustom.STYLE_LABEL_BOLD),
+              TextBuilder.build(title: "Tablet",style: TextStyleCustom.STYLE_LABEL_BOLD),
+              TextBuilder.build(title: "Cleaning",style: TextStyleCustom.STYLE_LABEL_BOLD),
+            ],
+          ),
+        ),
+        body: Container(
+          child: Center(child: TextBuilder.build(title: "History")),
+        ),
       ),
     );
   }
