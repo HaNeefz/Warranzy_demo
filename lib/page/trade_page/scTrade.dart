987e4e0dc29a61e3410014b4853127bbb6031ac4
HaNeefz <47207756+HaNeefz@users.noreply.github.com>
2019-07-02 08:54:49 +0700

    editTradePage

--- lib/page/trade_page/scTrade.dart
+++ lib/page/trade_page/scTrade.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:warranzy_demo/tools/assets.dart';
 import 'package:warranzy_demo/tools/config/text_style.dart';
 import 'package:warranzy_demo/tools/theme_color.dart';
 import 'package:warranzy_demo/tools/widget_ui_custom/text_builder.dart';
@@ -9,17 +10,7 @@ class TradePage extends StatefulWidget {
 }
 
 class _TradePageState extends State<TradePage> {
-  int _currentPage = 1;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
+  int _currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -57,11 +48,9 @@ class _TradePageState extends State<TradePage> {
               title: "Trade Market", style: TextStyleCustom.STYLE_APPBAR),
           bottom: TabBar(
             indicatorColor: COLOR_THEME_APP,
-            // controller: _tabController,
             onTap: (i) {
               setState(() {
                 _currentPage = i;
-                // _tabController.animateTo(_currentPage);
               });
             },
             labelPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
@@ -88,10 +77,34 @@ class _TradePageState extends State<TradePage> {
         body: Container(
           child: TabBarView(
             children: <Widget>[
-              TextBuilder.build(
-                  title: "Popular",
-                  style:
-                      TextStyleCustom.STYLE_LABEL_BOLD.copyWith(fontSize: 14)),
+              Scaffold(
+                backgroundColor: COLOR_GREY.withOpacity(0.3),
+                body: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: GridView.builder(
+                    itemCount: 10,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                        childAspectRatio: 6 / 9),
+                    itemBuilder: (BuildContext context, int index) {
+                      return ModelTrade(
+                        id: index,
+                        image: Image.asset(
+                          Assets.BACK_GROUND_APP,
+                          fit: BoxFit.cover,
+                        ),
+                        price: "1,000 B",
+                        title: "Dyson V7 Trigger",
+                        subTitle:
+                            "Simply dummy text of the printing and ba ba ba ba",
+                        category: "Cleaning",
+                      );
+                    },
+                  ),
+                ),
+              ),
               TextBuilder.build(
                   title: "Electronics",
                   style:
@@ -106,6 +119,95 @@ class _TradePageState extends State<TradePage> {
                       TextStyleCustom.STYLE_LABEL_BOLD.copyWith(fontSize: 14)),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class ModelTrade extends StatelessWidget {
+  final Widget image;
+  final int id;
+  final String price;
+  final String title;
+  final String subTitle;
+  final String category;
+
+  const ModelTrade(
+      {Key key,
+      this.image,
+      this.price,
+      this.title,
+      this.subTitle,
+      this.category,
+      this.id})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: COLOR_WHITE,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: GestureDetector(
+        onTap: () {
+          print(id);
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10.0),
+                        topRight: Radius.circular(10.0)),
+                    child: Hero(
+                      child: image,
+                      tag: "Image$id",
+                    )),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    TextBuilder.build(
+                        title: price, style: TextStyleCustom.STYLE_CONTENT),
+                    TextBuilder.build(
+                        title: title, style: TextStyleCustom.STYLE_LABEL_BOLD),
+                    TextBuilder.build(
+                        title: subTitle,
+                        style: TextStyleCustom.STYLE_CONTENT
+                            .copyWith(fontSize: 14),
+                        textOverflow: TextOverflow.ellipsis,
+                        maxLine: 2),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Container(
+                      // padding: EdgeInsets.only(top: 10.0),
+                      width: 100,
+                      height: 25,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: COLOR_GREY.withOpacity(0.3)),
+                      child: Center(
+                        child: TextBuilder.build(
+                            title: category,
+                            style: TextStyleCustom.STYLE_LABEL
+                                .copyWith(fontSize: 14)),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/tools/widget_ui_custom/text_builder.dart
+++ lib/tools/widget_ui_custom/text_builder.dart
@@ -2,16 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:warranzy_demo/tools/config/text_style.dart';
 
 class TextBuilder {
-  static Widget build(
-      {@required String title,
-      TextStyle style,
-      TextAlign textAlign,
-      TextOverflow textOverflow}) {
+  static Widget build({
+    @required String title,
+    TextStyle style,
+    TextAlign textAlign,
+    TextOverflow textOverflow,
+    int maxLine,
+  }) {
     return Text(
       title,
       style: style ?? TextStyleCustom.STYLE_LABEL,
       textAlign: textAlign,
-      overflow: textOverflow,      
+      overflow: textOverflow,
+      maxLines: maxLine,
     );
   }
 }
