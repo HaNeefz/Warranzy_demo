0a730b9635717b7eabf097313df866dc3e14850f
HaNeefz <47207756+HaNeefz@users.noreply.github.com>
2019-07-15 11:57:29 +0700

    edit oneSignal

--- lib/main.dart
+++ lib/main.dart
@@ -56,6 +56,16 @@ class _MyHomePageState extends State<MyHomePage> {
         .init("138414dc-cb53-43e0-bc67-49fc9b7a99f4", iOSSettings: settings);
     OneSignal.shared
         .setInFocusDisplayType(OSNotificationDisplayType.notification);
+    OneSignal.shared
+        .setNotificationReceivedHandler((OSNotification notification) {
+      // will be called whenever a notification is received
+      print("ReceiveHandler => ${notification.payload}");
+    });
+    OneSignal.shared
+        .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+      // will be called whenever a notification is opened/button pressed.
+      print("OpenedHandler $result");
+    });
   }
 
   // initnoti() async {
